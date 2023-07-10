c2be293abe850656731095643e185fab22d07c59
Erik Shahverdian <13771746+Demigod808@users.noreply.github.com>
2023-07-10 14:33:53 -0700

    Add files via upload
    Skeleton setup for a discord bot

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTEyODA1NzI3NTM5NTg3OTAwMw.GB8IAH.7r9RGTi_Ws1tKj9BOee2X1O0I4sK0jtOAERh3c'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
