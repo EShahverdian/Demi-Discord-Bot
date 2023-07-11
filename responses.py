c0519729de79bcfe879a2667a1b568cb1eaf7bf8
Erik Shahverdian <13771746+Demigod808@users.noreply.github.com>
2023-07-11 10:32:04 -0700

    Update responses.py
    added custom dice roll, custom responses for quotes.

--- responses.py
+++ responses.py
@@ -4,13 +4,24 @@ import random
 def get_response(message: str) -> str:
     p_message = message.lower()
 
-    if p_message == 'hello':
-        return 'Hey there!'
+    if p_message == '/quote':
+        x = (random.randint(1, 5))
+        if x == 1:
+            return 'Yeah whatever kid.'
+        if x == 2:
+            return 'Ight cya'
+        if x == 3:
+            return 'Theres no good games to play'
+        if x == 4:
+            return 'Wheres Birknasty!?'
+        if x == 5:
+            'He aint gon be in Rush hour 3'
 
-    if message == 'roll':
-        return str(random.randint(1, 6))
+    if message == '/roll':
+        return str(random.randint(1, 20))
 
-    if p_message == '!help':
-        return '`This is a help message that you can modify.`'
-
-    return 'I didn\'t understand what you wrote. Try typing "!help".'
+    if p_message == '/help':
+        return '`Here are the commands for Demi Discord Bot.\n' \
+               '/roll will roll a 20 sided dice\n' \
+               '/quote will quote Demi with one of his great sayings\n' \
+               '`'
